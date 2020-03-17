04a00456543052b4fb71869cbc051ef519c06a70
cade.gardner <d00299297@dmail.dixie.edu>
2020-03-17 13:18:00 -0600

    fixed error with icons

--- trailWaze.js
+++ trailWaze.js
@@ -1,14 +1,14 @@
  /*jshint esversion: 6 */
 
- var app = new Vue({
+var app = new Vue({
     el: '#app',
     data: {
 
     },
     methods: {
-        
+
     }
- });
+});
 
 // THIS SECTION NEEDED TO REGISTER SERVICE WORKER //
 window.onload = () => {
