2d50edbee11b7bd3305cafbb8b702d4a72c87665
treydinwinward <treydinw@gmail.com>
2020-03-17 13:41:46 -0600

    SVG working properly

--- trailWaze.js
+++ trailWaze.js
@@ -1,5 +1,32 @@
  /*jshint esversion: 6 */
 
+
+// Start of functions for the SVG circle
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+  
+function setStop(id, radius, stop){
+    var c = document.getElementById(id);
+    c.className = "background";
+    var stopVal = Math.PI * radius * 2 * (stop/10);
+    c.setAttribute("stroke-dasharray", stopVal + ", 3000");
+    c.setAttribute("stroke-dashoffset", stopVal);
+    c.className = "overlayLine";
+}
+  
+function randomStops(){
+    setStop("line1", 20, getRandomIntInclusive(1, 10));
+    setStop("line2", 30, getRandomIntInclusive(1, 10));
+    setStop("line3", 40, getRandomIntInclusive(1, 10));
+    setStop("line4", 50, getRandomIntInclusive(1, 10));
+}
+randomStops();
+// End of functions for SVG circle
+  
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -20,27 +47,3 @@ window.onload = () => {
     }
 };
 
-
-// functions for the SVG circle
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-  }
-  
-  function setStop(id, radius, stop){
-      var c = document.getElementById(id);
-      c.className = "background";
-      var stopVal = Math.PI * radius * 2 * (stop/10);
-      c.setAttribute("stroke-dasharray", stopVal + ", 3000");
-      c.setAttribute("stroke-dashoffset", stopVal);
-      c.className = "overlayLine";
-  }
-  
-  function randomStops(){
-      setStop("line1", 20, getRandomIntInclusive(1, 10));
-      setStop("line2", 30, getRandomIntInclusive(1, 10));
-      setStop("line3", 40, getRandomIntInclusive(1, 10));
-      setStop("line4", 50, getRandomIntInclusive(1, 10));
-  }
-  
