d1aa7cacf368846c6bb132d0b18c775c07cc3dac
cade.gardner <d00299297@dmail.dixie.edu>
2020-03-17 14:25:16 -0600

    implemented title, date, font

--- trailWaze.js
+++ trailWaze.js
@@ -3,10 +3,32 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+        todaysDate: "",
     },
     methods: {
-
+        getTodaysDate: function () {
+            var date = new Date();
+            var days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+            var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var hours = date.getHours();
+            var TOD;
+            if (hours > 12){
+                hours-=12;
+                TOD = "PM";
+            }else{
+                TOD = "AM";
+            }
+            var minutes = date.getMinutes();
+            if (minutes < 10){
+                minutes = "0" + date.getMinutes();
+            }
+            var fulldate = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDay() + " " + hours + ":" + minutes + " " + TOD;
+            console.log(fulldate);
+            this.todaysDate = fulldate;
+        }
+    },
+    mounted() {
+        this.getTodaysDate();
     }
 });
 
