54e585d0dc7cba732b8789f92d1cc592957e9099
treydinwinward <treydinw@gmail.com>
2020-03-17 13:25:54 -0600

    working on svg

--- trailWaze.js
+++ trailWaze.js
@@ -19,3 +19,28 @@ window.onload = () => {
                  .register('./sw.js');
     }
 };
+
+
+// functions for the SVG circle
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+  }
+  
+  function setStop(id, radius, stop){
+      var c = document.getElementById(id);
+      c.className = "background";
+      var stopVal = Math.PI * radius * 2 * (stop/10);
+      c.setAttribute("stroke-dasharray", stopVal + ", 3000");
+      c.setAttribute("stroke-dashoffset", stopVal);
+      c.className = "overlayLine";
+  }
+  
+  function randomStops(){
+      setStop("line1", 20, getRandomIntInclusive(1, 10));
+      setStop("line2", 30, getRandomIntInclusive(1, 10));
+      setStop("line3", 40, getRandomIntInclusive(1, 10));
+      setStop("line4", 50, getRandomIntInclusive(1, 10));
+  }
+  
